6a57808d9a27e67483d367f74601cb5b88123ae9
theirrationalone <anilkushwaha289@gmail.com>
2026-07-27 22:02:51 +0000

    Add transfer to IERC20

--- src/IERC20.sol
+++ src/IERC20.sol
@@ -8,4 +8,5 @@ interface IERC20 {
 
     function totalsupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
 }
