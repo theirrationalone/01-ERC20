17c8f9322650276a7ee5226bb28f118dcab3f107
theirrationalone <anilkushwaha289@gmail.com>
2026-07-27 22:35:01 +0000

    Add approve to IERC20

--- src/IERC20.sol
+++ src/IERC20.sol
@@ -10,4 +10,5 @@ interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 value) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
 }
