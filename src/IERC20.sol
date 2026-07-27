f893a648d3a68e4650412bf1b53f46fa2c4a6951
theirrationalone <anilkushwaha289@gmail.com>
2026-07-27 23:32:53 +0000

    Add transferFrom to IERC20

--- src/IERC20.sol
+++ src/IERC20.sol
@@ -11,4 +11,5 @@ interface IERC20 {
     function transfer(address to, uint256 value) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
