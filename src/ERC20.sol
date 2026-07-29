46f06d9770dfae6c29440facf1d65de1ec808c38
theirrationalone <anilkushwaha289@gmail.com>
2026-07-29 00:23:51 +0000

    Add _allowances to ERC20

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -6,4 +6,5 @@ import {IERC20} from "./IERC20.sol";
 
 contract ERC20 is IERC20 {
     mapping(address account => uint256 balance) private _balances;
+    mapping(address owner => mapping(address spender => uint256 allowance)) private _allowances;
 }
